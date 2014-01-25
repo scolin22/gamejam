d04a004b7d20585e6d0523816fb5495f88daaf46
Gavin <gnswebsitedesign@gmail.com>
2014-01-24 21:22:12 -0800

    Starting Day 5

--- Possession/src/possessiongame/MainClass.java
+++ Possession/src/possessiongame/MainClass.java
@@ -1,112 +1,162 @@
 package possessiongame;
 
+
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 public class MainClass extends Applet implements Runnable, KeyListener {
 
-    @Override
-    public void init() {
 
-        setSize(800, 480);
-        setBackground(Color.BLACK);
-        setFocusable(true);
-        addKeyListener(this);
-        Frame frame = (Frame) this.getParent().getParent();
-        frame.setTitle("Q-Bot Alpha");
-    }
+    private Image image, character;
+	private Graphics second;
+	private URL base;
+	
+	private Robot robot; //ADDED
 
-    @Override
-    public void start() {
-        Thread thread = new Thread(this);
-        thread.start();
-    }
+	@Override
+	public void init() {
 
-    @Override
-    public void stop() {
-        // TODO Auto-generated method stub
-    }
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
-    @Override
-    public void destroy() {
-        // TODO Auto-generated method stub
-    }
+		// Image Setups
+		character = getImage(base, "data/character.png");
 
-    @Override
-    public void run() {
-        while (true) {
+	}
 
-            repaint();
-            try {
-                Thread.sleep(17);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	@Override
+	public void start() {
+		robot = new Robot();
 
-    @Override
-    public void keyPressed(KeyEvent e) {
+		Thread thread = new Thread(this);
+		thread.start();
+	}
 
-        switch (e.getKeyCode()) {
-        case KeyEvent.VK_UP:
-            System.out.println("Move up");
-            break;
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
 
-        case KeyEvent.VK_DOWN:
-            System.out.println("Move down");
-            break;
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
 
-        case KeyEvent.VK_LEFT:
-            System.out.println("Move left");
-            break;
+	@Override
+	public void run() {
+		while (true) {
+			robot.update();
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-        case KeyEvent.VK_RIGHT:
-            System.out.println("Move right");
-            break;
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
 
-        case KeyEvent.VK_SPACE:
-            System.out.println("Jump");
-            break;
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
 
-        }
+		g.drawImage(image, 0, 0, this);
 
-    }
+	}
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        switch (e.getKeyCode()) {
-        case KeyEvent.VK_UP:
-            System.out.println("Stop moving up");
-            break;
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, robot.getCenterX() - 61, robot.getCenterY() - 63, this);
 
-        case KeyEvent.VK_DOWN:
-            System.out.println("Stop moving down");
-            break;
+	}
 
-        case KeyEvent.VK_LEFT:
-            System.out.println("Stop moving left");
-            break;
+	@Override
+	public void keyPressed(KeyEvent e) {
 
-        case KeyEvent.VK_RIGHT:
-            System.out.println("Stop moving right");
-            break;
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
 
-        case KeyEvent.VK_SPACE:
-            System.out.println("Stop jumping");
-            break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			
+			break;
 
-        }
+		case KeyEvent.VK_LEFT:
+			System.out.println("Move left");
+			robot.moveLeft();
+			break;
 
-    }
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Move right");
+			robot.moveRight();
+			break;
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			robot.jump();
+			break;
 
-    }
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop moving left");
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop moving right");
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
+			break;
+
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
