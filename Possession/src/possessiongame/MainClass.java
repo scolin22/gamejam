26164c076579bb19d8b6547e0f4b72980ca4053b
ijcheng <ice.7000@gmail.com>
2014-01-25 17:54:42 -0800

    Loop theme music

--- Possession/src/possessiongame/MainClass.java
+++ Possession/src/possessiongame/MainClass.java
@@ -13,12 +13,18 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MainClass extends Applet implements Runnable, KeyListener {
 
@@ -81,6 +87,24 @@ public class MainClass extends Applet implements Runnable, KeyListener {
 			e.printStackTrace();
 		}
 	     // The above line throws an checked IOException which must be caught.
+		
+		try {
+	         // Open an audio input stream.
+	         URL url = this.getClass().getClassLoader().getResource("data/PossessionMain2.wav");
+	         AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+	         // Get a sound clip resource.
+	         Clip clip = AudioSystem.getClip();
+	         // Open audio clip and load samples from the audio input stream.
+	         clip.open(audioIn);
+	         clip.start();
+	         clip.loop(Clip.LOOP_CONTINUOUSLY);
+	      } catch (UnsupportedAudioFileException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (LineUnavailableException e) {
+	         e.printStackTrace();
+	      }
 		
 		character_backwards = bigImg.getSubimage( 0, 0, 14, 19 );
 		character_backwards2 = bigImg.getSubimage( 14, 0, 15, 19 );
